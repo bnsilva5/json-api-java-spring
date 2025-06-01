eb8ce705095637342b30d422db64e077147a8f98
Nicholas Silva Ruiz <brandon.silva@empicolsa.com>
2025-06-01 12:06:34 -0500

    Configuracion inicial y microservicio de producto

---
+++ product-service/src/main/java/com/nicholassr/product_service/dtos/ProductDto.java
@@ -0,0 +1,19 @@
+package com.nicholassr.product_service.dtos;
+
+import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Type;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data // De Lombok: genera getters, setters, toString, equals, hashCode
+@NoArgsConstructor // De Lombok: genera constructor sin argumentos
+@AllArgsConstructor // De Lombok: genera constructor con todos los argumentos
+@Type("products")
+public class ProductDto {
+    @Id
+    private String name;
+    private BigDecimal price;
+}
